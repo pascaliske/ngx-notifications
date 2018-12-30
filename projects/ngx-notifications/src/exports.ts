d57ccccf31fdfc67217c6775c39fe86825eac557
Pascal Iske <info@pascal-iske.de>
2018-12-30 14:20:46 +0100

    chore(ng-packagr): rename entry file

--- projects/ngx-notifications/src/exports.ts
+++ projects/ngx-notifications/src/exports.ts
@@ -1,8 +1,8 @@
 /*
  * Public API Surface
  */
-export * from './lib/notifications.service'
-export * from './lib/notifications.component'
 export * from './lib/notifications.module'
+export * from './lib/notifications.component'
+export * from './lib/notifications.service'
 export * from './lib/notification'
 export { NotificationOptions } from './lib/typings'
